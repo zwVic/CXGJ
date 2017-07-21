72ac29a03085f59472d0dff6dcfb4cab5a2368e2
zwVic <3493455038@qq.com>
2017-07-21 15:30:08 +0800

    zw want to fix fy bug

--- modules/Hotel/ht-index/js/ht-index.js
+++ modules/Hotel/ht-index/js/ht-index.js
@@ -7,11 +7,9 @@ var picSwiper = new Swiper('.pic-banner', {
     paginationClickable: '.swiper-pagination',  //分页器是否可以点击
     nextButton: '.swiper-button-next',          //下一张按钮
     prevButton: '.swiper-button-prev',          //上一张按钮
-    effect: 'cube',         //切换效果
+    effect: 'flip',         //切换效果
     loop: true,             //自动循环
     autoplay : 5000,        //自动播放
-    slidesPerView: 3,
-    centeredSlides: true,
 });
 
 //文字轮播
@@ -21,3 +19,32 @@ var textSwiper = new Swiper('.text-banner', {
     loop: true,
     autoplay : 3000,
 });
+
+//介绍部分右边轮播
+
+var introSwiper = new Swiper('.intro-banner', {
+    spaceBetween: 30,
+    effect: 'fade',
+    pagination: '.swiper-pagination',           //分页器
+    paginationClickable: '.swiper-pagination',  //分页器是否可以点击
+    nextButton: '.swiper-button-next',          //下一张按钮
+    prevButton: '.swiper-button-prev',          //上一张按钮
+    loop: true,
+    autoplay : 2000
+});
+
+(function () {
+    var imgLoading = false;
+    var arr = ["one","two","three","four","five"];
+    $(document).scroll(function(){
+        if(!imgLoading) {
+
+            if ($(document).scrollTop()+ $(window).height()>=$(".introduction-container").position().top) {
+                for(var i=0;i<arr.length;i++){
+                   $(".right").find(".loading-"+arr[i]).addClass("intro-banner-"+arr[i]).removeClass("loading-"+arr[i]);
+                }
+                imgLoading = true;
+            }
+        }
+    })
+})()
