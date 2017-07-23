2c5d29659c768d725f2a5e8a027da101ee1af044
fengyong <373209197@qq.com>
2017-07-23 11:07:15 +0800

    fy push in 07/23 11:07

--- entry/Hotel/ht-index.js
+++ entry/Hotel/ht-index.js
@@ -4,6 +4,7 @@
 //公共js scss
 require("modules/Hotel/common/js/hotel-header")
 require("modules/Hotel/common/scss/hotel-header.scss")
+
 //swiper
 require("modules/common/js/swiper-3.4.2.min");
 require("modules/common/scss/swiper-3.4.2.min.css")
