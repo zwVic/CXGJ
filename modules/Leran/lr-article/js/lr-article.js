024ffefc27de46af33844b2b23c8eefaf13715f1
zwVic <3493455038@qq.com>
2017-08-01 12:56:40 +0800

    8/1

--- modules/Leran/lr-article/js/lr-article.js
+++ modules/Leran/lr-article/js/lr-article.js
@@ -3,37 +3,38 @@
  */
 (function ($) {
 
-    var searchURL = window.location.search;
-    var searchURL = searchURL.substring(1,searchURL.length).split("&");
-    var type = searchURL[0].split("=")[1];
-    var  id= searchURL[1].split("=")[1];
-    switch (type){
-        case 'theory':
-            $("header .item:eq(1) li:eq(0)").addClass("active");
-            break;
-        case 'experiment':
-            $("header .item:eq(1) li:eq(1)").addClass("active");
-            break;
-        case 'theatre':
-            $("header .item:eq(1) li:eq(2)").addClass("active");
-            break;
-        case 'notes':
-            $("header .item:eq(1) li:eq(3)").addClass("active");
-            break;
-
-
-    }
+    // var searchURL = window.location.search;
+    // var searchURL = searchURL.substring(1,searchURL.length).split("&");
+    // var type = searchURL[0].split("=")[1];
+    // var  id= searchURL[1].split("=")[1];
+    // switch (type){
+    //     case 'theory':
+    //         $("header .item:eq(1) li:eq(0)").addClass("active");
+    //         break;
+    //     case 'experiment':
+    //         $("header .item:eq(1) li:eq(1)").addClass("active");
+    //         break;
+    //     case 'theatre':
+    //         $("header .item:eq(1) li:eq(2)").addClass("active");
+    //         break;
+    //     case 'notes':
+    //         $("header .item:eq(1) li:eq(3)").addClass("active");
+    //         break;
+    //
+    //
+    // }
    $.ajax({
-        'url':,
+        'url':'http://127.0.0.1:3000/articles/lrArticle/find',
         type:'get',
         data:{
-            'id':id
+            _id:"597ee852681a3c1c2420fb56"
         },
         success:function(data){
-            $(".main .info .title").html(data.title);
-            $(".main .info .author").html(data.author);
-            $(".main .info .time").html(data.time);
-            $(".main .article").append(data.content);
+            console.log(data);
+            $(".main .info .title").html(data.data.title);
+            $(".main .info .author").html(data.data.author);
+            $(".main .info .time").html(data.data.time);
+            $(".main .article").append(data.data.content);
         }
     })
 })(window.jQuery)
