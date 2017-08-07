afc3bc2a1782984a8073091b2bb2cffd7e23f501
zwVic <3493455038@qq.com>
2017-08-07 14:41:58 +0800

    zw

--- entry/Leran/lr-articles.js
+++ entry/Leran/lr-articles.js
@@ -3,6 +3,7 @@
 //  */
 require("Leran/common/js/lr-header")
 require("Leran/common/scss/lr-header.scss")
+require("Leran/common/scss/lr-template.scss")
 
 require("Leran/lr-articles/js/lr-pagination.js")
 
