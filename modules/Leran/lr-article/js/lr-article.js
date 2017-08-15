af61fec2bf5141e37997d09f76406a6624cee041
zwStar <979904565@qq.com>
2017-08-15 17:10:48 +0800

    after work add eight articles to xinli xueyuan

--- modules/Leran/lr-article/js/lr-article.js
+++ modules/Leran/lr-article/js/lr-article.js
@@ -6,12 +6,12 @@
     var wheelrate = 30;
     var position = 0;
     var lastposition = 0;
-    $(".content-wrapper").on("mousewheel DOMMouseScroll",function (e) {
+    $(".content-wrapper").on("mousewheel DOMMouseScroll", function (e) {
         var oev = e.originalEvent;
         lastposition = position;
         position = (oev.wheelDelta ? -oev.wheelDelta / 120 : (oev.detail / 3)) * wheelrate + this.scrollTop;
         $(this).scrollTop(position);
-        if(lastposition != position)
+        if (lastposition != position)
             return false;
     })
 
@@ -20,32 +20,48 @@
     var idREG = /id=([\w]+)&?/;
     var typeREG = /type=([a-zA-Z]+)&?/;
     var type = searchURL.match(typeREG)[1];
+    changeActive(type);
     var id = searchURL.match(idREG)[1];
 
-    switch (type){
-        case 'theory':
-            $("header .item:eq(1) li:eq(0)").addClass("active");
-            break;
-        case 'experiment':
-            $("header .item:eq(1) li:eq(1)").addClass("active");
-            break;
-        case 'theatre':
-            $("header .item:eq(1) li:eq(2)").addClass("active");
-            break;
-        case 'notes':
-            $("header .item:eq(1) li:eq(3)").addClass("active");
-            break;
-    
-    
+    // switch (type){
+    //     case 'theory':
+    //         $("header .item:eq(1) li:eq(0)").addClass("active");
+    //         break;
+    //     case 'experiment':
+    //         $("header .item:eq(1) li:eq(1)").addClass("active");
+    //         break;
+    //     case 'theatre':
+    //         $("header .item:eq(1) li:eq(2)").addClass("active");
+    //         break;
+    //     case 'notes':
+    //         $("header .item:eq(1) li:eq(3)").addClass("active");
+    //         break;
+    //
+    //
+    // }
+
+    function changeActive(type) {
+
+        var li = $(".navList").find("li");
+
+        for (var i = 0; i < li.length; i++) {
+            var reType = /type=([\w]+)&?/;
+            if (li.eq(i).find("a")[0].href.match(reType)[1] === type) {
+                li.eq(i).addClass("active").siblings().removeClass("active");
+                break;
+            }
+        }
+
     }
-    
-   $.ajax({
-        url:"http://127.0.0.1:3000/articles/lrArticle/find",
-        type:'get',
-        data:{
-            _id:id
+
+
+    $.ajax({
+        url: "http://127.0.0.1:3000/articles/lrArticle/find",
+        type: 'get',
+        data: {
+            _id: id
         },
-        success:function(data){
+        success: function (data) {
             $(" .info .title").html(data.data.title);
             $(" .info .author").html(data.data.publisher);
             $(" .info .time").html(data.data.date);
